0fa7c0b7039fdd4725e879941ebb64a40e012f02
averagejoesec <gonzalezjlusmc@gmail.com>
2025-12-18 14:42:05 +0100

    using variables

--- practice.cs
+++ practice.cs
@@ -4,3 +4,9 @@ Console.Write("You wrote your first lines of code.");
 Console.WriteLine();
 Console.WriteLine("This is the first line.");
 Console.Write("This is the second line.");
+
+// Using variables
+string firstName = "Bob";
+int messages = 3;
+float temperature = 34.4f;
+Console.Write($"Hello, {firstName}! You have {messages} messages in your inbox. The temperature is {temperature} celsius.");
