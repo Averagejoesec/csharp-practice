329e5d398c8bac4c0ed6ec4b79a7f6688fd7d207
averagejoesec <gonzalezjlusmc@gmail.com>
2025-12-23 21:55:06 +0100

    use StartsWith to filter

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,4 +1,14 @@
-﻿int firstvalue = 500;
-int secondvalue = 1000;
-int largerValue = Math.Max(firstvalue, secondvalue);
-Console.WriteLine("The larger value is: " + largerValue);
+﻿// int firstvalue = 500;
+// int secondvalue = 1000;
+// int largerValue = Math.Max(firstvalue, secondvalue);
+// Console.WriteLine("The larger value is: " + largerValue);
+
+// Using StartsWith to filter and print orders that start with 'B'
+string[] orders = [ "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" ];
+foreach ( string order in orders)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
+}
